92453f57e0c9e7e647d64cb57792382f2be2a0fb
admix <admix.snurnikov@gmail.com>
2014-05-17 00:30:53 -0400

    Updated server ports, added env file and updated readme

--- web_static.js
+++ web_static.js
@@ -1,7 +1,10 @@
 var express = require('express'),
     app = express();
 
+app.set('port', (process.env.PORT || 8080))
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080);
-
+app.listen(app.get('port'), function() {
+  console.log("Listening on port: " + app.get('port'))
+});
