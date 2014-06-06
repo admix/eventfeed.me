a5755701405eec1800ecacc1a639551ab7f34ee4
admix <admix.snurnikov@gmail.com>
2014-06-05 23:46:49 -0400

    Added 404 error handler

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var app = express(),
 
 app.use(express.static(__dirname + "/public"));
 
+app.use(function(req, res, next){
+  res.send(404, 'Sorry cant find that!');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
+
 app.listen(port, function() {
   console.log("Listening on port: " + port);
 });
